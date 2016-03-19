c98f9191a1d86f84fe905e6a84f8ca55b7fc7014
Philippe Proulx <eeppeliteloop@gmail.com>
2016-03-18 20:15:43 -0400

    barectf v2.1.0
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- barectf/__init__.py
+++ barectf/__init__.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2014-2015 Philippe Proulx <pproulx@efficios.com>
+# Copyright (c) 2014-2016 Philippe Proulx <pproulx@efficios.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-__version__ = '2.1.0-dev'
+__version__ = '2.1.0'
 
 
 def _split_version_suffix():
