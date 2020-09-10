b1c5cbcf3b6196cac7a813507b31c11190fef4f4
Philippe Proulx <eeppeliteloop@gmail.com>
2020-09-10 00:06:41 -0400

    tests/config/yaml: use pytest_collect_file() hook for YAML files
    This patch changes `tests/config/yaml/conftest.py` so that it implements
    a pytest_collect_file() Pytest hook to create Pytest file and item
    objects for each failing barectf YAML configuration file found.
    
    This makes is possible to remove all `test_fail_*.py` files and avoid
    duplication and mismatches between existing YAML files and available
    test functions.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- tests/config/yaml/conftest.py
+++ tests/config/yaml/conftest.py
@@ -26,13 +26,65 @@ import os.path
 import barectf
 
 
-@pytest.fixture
-def config_fail_test(yaml_cfg_path):
-    def func():
-        yaml_dir = os.path.dirname(yaml_cfg_path)
+def pytest_collect_file(parent, path):
+    yaml_ext = '.yaml'
 
-        with open(yaml_cfg_path) as f:
+    if path.ext != yaml_ext:
+        # not a YAML file: cancel
+        return
+
+    # At the end of this loop, if `path` is
+    # `/home/jo/barectf/tests/config/yaml/2/configs/fail/stream/pct-no.yaml`,
+    # for example, then `elems` is:
+    #
+    # * `pct-no.yaml`
+    # * `stream`
+    # * `fail`
+    path_str = str(path)
+    elems = []
+
+    while True:
+        elem = os.path.basename(path_str)
+
+        if elem == 'configs':
+            break
+
+        elems.append(elem)
+        path_str = os.path.dirname(path_str)
+
+    if 'fail' not in elems:
+        return
+
+    # create a unique test name
+    name = f'test-{"-".join(reversed(elems))}'.replace(yaml_ext, '')
+
+    # create the file node
+    return _YamlFile.from_parent(parent, fspath=path, name=name)
+
+
+class _YamlFile(pytest.File):
+    def __init__(self, parent, fspath, name):
+        super().__init__(parent=parent, fspath=fspath)
+        self._name = name
+
+    def collect(self):
+        # yield a single item
+        yield _YamlItem.from_parent(self, name=self._name)
+
+
+class _YamlItem(pytest.Item):
+    def __init__(self, parent, name):
+        super().__init__(parent=parent, name=name)
+
+    def runtest(self):
+        yaml_dir = self.fspath.dirname
+
+        with open(str(self.fspath)) as f:
             with pytest.raises(barectf._ConfigurationParseError):
                 barectf.configuration_from_file(f, inclusion_directories=[yaml_dir])
 
-    return func
+    def repr_failure(self, excinfo, style=None):
+        return f'`{self.fspath}` did not make barectf.configuration_from_file() raise `barectf._ConfigurationParseError`: {excinfo}.'
+
+    def reportinfo(self):
+        return self.fspath, None, self.name
