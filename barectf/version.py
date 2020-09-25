ce6142f074ba1454a966ad0acc4f9a66464a725e
Philippe Proulx <eeppeliteloop@gmail.com>
2020-09-25 15:19:40 -0400

    Prepare for barectf v3.0.1
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- barectf/version.py
+++ barectf/version.py
@@ -23,6 +23,6 @@
 
 __major_version__ = 3
 __minor_version__ = 0
-__patch_version__ = 0
-__pre_version__ = ''
+__patch_version__ = 1
+__pre_version__ = '-dev'
 __version__ = f'{__major_version__}.{__minor_version__}.{__patch_version__}{__pre_version__}'
