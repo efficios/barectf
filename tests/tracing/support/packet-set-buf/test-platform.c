0a5733ae7584859cc36092a093acaac876efcd16
Philippe Proulx <eeppeliteloop@gmail.com>
2020-10-06 14:20:12 -0400

    Fix: barectf_packet_set_buf(): keep full packet state
    The condition of barectf_packet_is_full() is:
    
        ctx->at == ctx->packet_size
    
    So we're reusing `ctx->at` to know whether or not a packet is full.
    
    A full packet is always (already) closed.
    
    The problem is that barectf_packet_set_buf() changes `ctx->packet_size`
    without touching `ctx->at`. Therefore if the value of `ctx->packet_size`
    changes, and if the packet was considered to be full, it's not the case
    anymore, but it must be.
    
    In barectf_packet_set_buf(), set `ctx->at` to `ctx->packet_size` if it
    was already the case to keep any full packet state.
    
    Adding a test with a custom platform calling barectf_packet_set_buf()
    with a `NULL` buffer after closing a completely full packet. Without
    this patch (in `barectf.c.j2`), this test leads to a segmentation fault.
    
    Fixes: https://github.com/efficios/barectf/issues/18
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ tests/tracing/support/packet-set-buf/test-platform.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2020 Philippe Proulx <pproulx@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+#include "barectf.h"
+#include "test-platform.h"
+
+struct test_platform_ctx {
+	struct barectf_default_ctx ctx;
+	uint8_t *buf;
+	size_t buf_size;
+	FILE *fh;
+};
+
+static void write_packet(struct test_platform_ctx * const platform_ctx)
+{
+	const size_t nmemb = fwrite(barectf_packet_buf(&platform_ctx->ctx),
+		barectf_packet_buf_size(&platform_ctx->ctx), 1,
+			platform_ctx->fh);
+
+	assert(nmemb == 1);
+}
+
+static int is_backend_full(void * const data)
+{
+	struct test_platform_ctx * const platform_ctx = (void *) data;
+
+	barectf_packet_set_buf(&platform_ctx->ctx, platform_ctx->buf,
+		platform_ctx->buf_size);
+	return 0;
+}
+
+static void open_packet(void * const data)
+{
+	struct test_platform_ctx * const platform_ctx = (void *) data;
+
+	memset(barectf_packet_buf(&platform_ctx->ctx), 0,
+		barectf_packet_buf_size(&platform_ctx->ctx));
+	barectf_default_open_packet(&platform_ctx->ctx);
+}
+
+static void close_packet(void * const data)
+{
+	struct test_platform_ctx * const platform_ctx = (void *) data;
+
+	barectf_default_close_packet(&platform_ctx->ctx);
+	write_packet(platform_ctx);
+	barectf_packet_set_buf(&platform_ctx->ctx, NULL, 512);
+}
+
+struct test_platform_ctx *test_platform_init(const size_t buf_size)
+{
+	struct test_platform_ctx *platform_ctx;
+	struct barectf_platform_callbacks cbs;
+
+	cbs.is_backend_full = is_backend_full;
+	cbs.open_packet = open_packet;
+	cbs.close_packet = close_packet;
+	platform_ctx = malloc(sizeof(*platform_ctx));
+	assert(platform_ctx);
+	platform_ctx->buf_size = buf_size;
+	platform_ctx->buf = malloc(platform_ctx->buf_size);
+	assert(platform_ctx->buf);
+	platform_ctx->fh = fopen("stream", "wb");
+	assert(platform_ctx->fh);
+	barectf_init(&platform_ctx->ctx, platform_ctx->buf,
+		platform_ctx->buf_size, cbs, platform_ctx);
+	open_packet(platform_ctx);
+	return platform_ctx;
+}
+
+void test_platform_fini(struct test_platform_ctx * const platform_ctx)
+{
+	if (barectf_packet_is_open(&platform_ctx->ctx) &&
+			!barectf_packet_is_empty(&platform_ctx->ctx)) {
+		close_packet(platform_ctx);
+	}
+
+	fclose(platform_ctx->fh);
+	free(platform_ctx->buf);
+	free(platform_ctx);
+}
+
+struct barectf_default_ctx *test_platform_barectf_ctx(
+	struct test_platform_ctx * const platform_ctx)
+{
+	return &platform_ctx->ctx;
+}
